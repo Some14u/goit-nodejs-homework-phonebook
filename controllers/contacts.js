d853e8ead171c77e469877ae2eaef98304e7cc9c
Some14u <some1@email.ua>
2023-01-25 19:17:39 +0200

    Refactor according to mentor review

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,16 +2,35 @@ const express = require("express");
 const router = express.Router();
 
 const service = require("../services/contacts");
-const handlers = require("./common").wrapWithHandlers(service);
 
-router //
-  .route("/")
-  .get(handlers.getAll)
-  .post(handlers.add);
-router //
-  .route("/:id")
-  .get(handlers.getById)
-  .delete(handlers.remove)
-  .put(handlers.update);
+  /**
+   * @param {import("express").Request} req
+   * @param {import("express").Response} res
+   */
+const listContactsHandler = async (req, res) => {
+  service.getAll();
+  res.
+}
 
-module.exports = router;
+  /**
+   * @param {import("express").Request} req
+   * @param {import("express").Response} res
+   */
+const getContactByIdHandler = async (req, res) => {
+  const { id } = req.params.id;
+
+}
+
+const removeContactHandler = async (req, res) => {}
+
+const addContactHandler = async (req, res) => {}
+
+const updateContactHandler = async (req, res) => { }
+
+module.exports = {
+  listContacts: listContactsHandler,
+  getContactById: getContactByIdHandler,
+  removeContact: removeContactHandler,
+  addContact: addContactHandler,
+  updateContact: updateContactHandler
+};
