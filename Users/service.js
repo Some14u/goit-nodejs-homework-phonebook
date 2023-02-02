e6ae34537d6a0768bf73c4aee421165bc3914d89
Some1 <some1@email.ua>
2023-02-02 20:17:47 +0200

    minor

--- Users/service.js
+++ Users/service.js
@@ -1,71 +1,41 @@
 const { ExistError, NotFoundError } = require("../helpers/errors");
-const Contact = require("./model");
+const User = require("./model");
 
 /**
- * Searches a contact matching id
- * @param {Number} id  id of contact to be returned
- * @returns {Contact} the contact with provided id
+ * Searches a user matching email
+ * @param {string} email an email of cuser to be returned
+ * @returns {Object} the user
  */
-async function getByEmailOrThrow(id) {
-  return await Contact.findById(id).orFail(new NotFoundError());
+async function getByEmailOrThrow(email) {
+  return await User.findOne({ email }).orFail(new NotFoundError());
 }
 
 /**
- * Removes a contact matching id
- * @param {Number} id id of contact to be removed
- */
-async function removeByIdOrThrow(id) {
-  await Contact.findByIdAndDelete(id).orFail(new NotFoundError());
-}
-
-/**
- * Adds a new contact using data, provided. The **id** is generated internally.
- * Checks the database for anoter contact with the same name.
- * @param params an object, containing **name**, **email**, and **phone** fields (**favorite** is optional).
+ * Adds a new user using data, provided
+ * Checks the database for anoter user with the same name.
+ * @param params an object, containing **email** and **password** fields
+ * (**subscription** and **token** are optional).
  */
 async function addOrThrow(params) {
-  const match = await Contact.findOne(Contact.filterByNameQuery(params.name));
-  if (match) throw new ExistError(params.name);
-  const contact = new Contact(params);
-  await contact.save();
-  return contact;
+  return await User.create(params)
 }
 
 /**
- * Updates existing contact matching id, using data, provided.
- * Checks the database for anoter contact with the same name.
- * @param {Number} id id of contact to be updated
- * @param params an object, containing **name**, **email**, and **phone** fields (**favorite** is optional).
- */
-async function updateByIdOrThrow(id, params) {
-  // Checking if we have "name" parameter and if so, then also checking,
-  // if there is the same name already in the database(with another id)
-  const match = await Contact.findOne(Contact.filterByNameQuery(params.name));
-  if (match && match.id !== id) throw new ExistError(params.name);
-  return await Contact.findByIdAndUpdate(id, params, { new: true }).orFail(
-    new NotFoundError()
-  );
-}
-
-/**
- * Updates existing contact favorite value.
+ * Updates existing user subscription value.
  * @param {Number} id id of contact to be updated
  * @param {Object} param
- * @param {boolean} param.favorite a new value
+ * @param {User.subscriptionTypes} param.subscription a new value
+ * @returns {Object} the user
  */
-async function updateStatusContact(id, { favorite }) {
-  return await Contact.findByIdAndUpdate(
+async function updateUserSubscription(id, { subscription }) {
+  return await User.findByIdAndUpdate(
     id,
-    { favorite },
+    { subscription },
     { new: true }
   ).orFail(new NotFoundError());
 }
 
 module.exports = {
-  getAllOrThrow,
-  getByIdOrThrow,
-  removeByIdOrThrow,
+  getByEmailOrThrow,
   addOrThrow,
-  updateByIdOrThrow,
-  updateStatusContact,
 };
