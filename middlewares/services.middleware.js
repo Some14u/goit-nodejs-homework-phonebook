88d2d812e6b6d2f3d927fe4b722fd69ce5c19f09
Some14u <some1@email.ua>
2023-02-06 05:06:11 +0200

    Rename size to limit, add some descriptions

--- middlewares/services.middleware.js
+++ middlewares/services.middleware.js
@@ -1,7 +1,15 @@
 const ContactService = require("../services/contact.service");
 const UserService = require("../services/user.service");
 
-/** @type {import("express").RequestHandler} */
+/**
+ * Services are Classes, and this instantiates them.
+ * Thus it is possible to link certain service settings
+ * directly to the current request context.
+ *
+ * I use this to link authenticated user id, which allows to
+ * use services without need to pass user id explicitly.
+ * @type {import("express").RequestHandler}
+ */
 function instantiateServices(req, __, next) {
   const services = {
     contact: new ContactService(),
