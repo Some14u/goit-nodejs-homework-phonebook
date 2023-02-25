750f24480e24069fe694fabcc535785f19f666ec
Some14u <some1@email.ua>
2023-02-25 16:55:44 +0200

    Partially implement jest testing

--- server.js
+++ server.js
@@ -1,44 +1,61 @@
 const settings = require("./helpers/settings");
+const messages = require("./helpers/messages");
+const errors = require("./helpers/errors");
 
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
+const db = require("./db/db");
+
 const contactsRouter = require("./routers/contact.router");
 const usersRouter = require("./routers/user.router");
 
-const messages = require("./helpers/messages");
-const errors = require("./helpers/errors");
-
-const connectMongoDB = require("./db/connection");
 const instantiateServices = require("./middlewares/services.middleware");
 
-connectMongoDB()
-  .then(startServer)
-  .catch(errors.showErrorAndStopApp(messages.unhandledError));
+const formatsLogger = settings.isDev ? "dev" : "short";
 
-function startServer() {
-  const app = express();
-  const formatsLogger = settings.isDev ? "dev" : "short";
+const instance = express()
+  .use(logger(formatsLogger))
+  .use(cors())
+  .use(express.urlencoded({ extended: false }))
+  .use(express.json())
 
-  app
-    .use(logger(formatsLogger))
-    .use(cors())
-    .use(express.urlencoded({ extended: false }))
-    .use(express.json())
+  .use(express.static(settings.folders.public))
 
-    .use(express.static(settings.folders.public))
+  .use(instantiateServices)
 
-    .use(instantiateServices)
+  .use("/api/contacts", contactsRouter)
+  .use("/users", usersRouter)
 
-    .use("/api/contacts", contactsRouter)
-    .use("/users", usersRouter)
+  .use(errors.globalNotFoundHandler)
+  .use(errors.globalErrorHandler);
 
-    .use(errors.globalNotFoundHandler)
-    .use(errors.globalErrorHandler)
+async function start() {
+  if (!instance) throw new Error(messages.server.noServerInstance);
 
+  await db.connect(settings.databaseConnectionString);
+
+  return instance
     .listen(settings.serverPort, () =>
-      console.log(messages.serverRunning(settings.serverPort))
+      console.log(messages.server.isRunning(settings.serverPort))
     )
     .on("error", errors.showErrorAndStopApp(messages.unhandledError));
 }
+
+/** @param {import('http').Server} instance */
+async function stop(instance, cb) {
+  if (!instance) cb?.(new Error(messages.server.noServerInstance));
+  if (!instance?.close) cb?.(new Error(messages.server.isNotRunning));
+
+  instance?.close?.(async (err) => {
+    await db.disconnect();
+    cb?.(err);
+  });
+}
+
+module.exports = {
+  instance,
+  start,
+  stop,
+};
